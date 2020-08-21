037ba641e4b92ce8715e0f126654e198ea9b2868
Moritz Sokoll <moritz@sokoll.com>
2020-08-21 17:40:40 +0200

    worked on desk software

--- desk/frontend.py
+++ desk/frontend.py
@@ -1,9 +1,19 @@
 import tkinter as tk
+import backend as bck
 root = tk.Tk()
 #init
 title = tk.Label(root, text="Noten Rechner")
 fach = tk.Label(root, text="Fach")
+mark = tk.Radiobutton(root, text="Neue Note", command=bck.newmark)
+newsubject = tk.Radiobutton(root, text="Neues Fach", command=bck.newsubject)
+marklab = tk.Label(root, text="Note")
+subjectlab = tk.Label(root, text="Fach")
+submit = tk.Button(root, text="Submit", bg="green")
+entry = tk.Entry(root, width=20)
+
 #grid
 title.grid(row=0, column=0)
-fach.grid(row=0, column=1)
+fach.grid(row=0, column=2)
+mark.grid(row=1, column=2)
+newsubject.grid(row=2, column=2)
 root.mainloop()
